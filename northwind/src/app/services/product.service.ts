26c9ecd4dc5c0e3fd8df91b9bcc40b1dda04d2c4
Onur Akkepenek <152120171023@ogrenci.ogu.edu.tr>
2021-04-04 00:25:39 +0300

    Initial commit

---
+++ northwind/src/app/services/product.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ProductResponseModel } from '../models/productResponceModel';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProductService {
+  constructor(private httpClient: HttpClient) {}
+
+  apiUrl = 'https://localhost:44383/api/products/getall';
+
+  getProducts(): Observable<ProductResponseModel> {
+    return this.httpClient.get<ProductResponseModel>(this.apiUrl);
+  }
+}
