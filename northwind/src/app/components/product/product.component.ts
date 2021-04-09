4198711c505bccbacc6b2ed08a69c8174570dc88
Onur Akkepenek <152120171023@ogrenci.ogu.edu.tr>
2021-04-09 16:45:54 +0300

    Add pipe and update components

--- northwind/src/app/components/product/product.component.ts
+++ northwind/src/app/components/product/product.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { Product } from 'src/app/models/product';
 import { ProductResponseModel } from 'src/app/models/productResponceModel';
+import { CartService } from 'src/app/services/cart.service';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
@@ -12,31 +14,44 @@ import { ProductService } from 'src/app/services/product.service';
 export class ProductComponent implements OnInit {
   products: Product[] = [];
   dataLoaded = false;
+  filterText = '';
+
   constructor(
     private productService: ProductService,
-    private activatedRoute: ActivatedRoute
+    private activatedRoute: ActivatedRoute,
+    private toastrService: ToastrService,
+    private cartService: CartService
   ) {}
 
   ngOnInit(): void {
-    this.activatedRoute.params.subscribe(params=>{
-      if(params["categoryId"]){
-        this.getProductsByCategory(params["categoryId"])
-      }
-      else{
+    this.activatedRoute.params.subscribe((params) => {
+      if (params['categoryId']) {
+        this.getProductsByCategory(params['categoryId']);
+      } else {
         this.getProducts();
       }
-    })
+    });
   }
   getProducts() {
     this.productService.getProducts().subscribe((response) => {
       this.products = response.data;
       this.dataLoaded = true;
     });
-  } 
-  getProductsByCategory(categoryId:number) {
-    this.productService.getProductsByCategory(categoryId).subscribe((response) => {
-      this.products = response.data;
-      this.dataLoaded = true;
-    });
+  }
+  getProductsByCategory(categoryId: number) {
+    this.productService
+      .getProductsByCategory(categoryId)
+      .subscribe((response) => {
+        this.products = response.data;
+        this.dataLoaded = true;
+      });
+  }
+  addToCart(product: Product) {
+    if (product.productId === 1) {
+      this.toastrService.error('Error', 'This product did not add.   ');
+    } else {
+      this.toastrService.success('Added cart', product.productName);
+      this.cartService.addToCart(product);
+    }
   }
 }
