eee0ae0b856a3319664e9939850af78e811d56ee
Onur Akkepenek <152120171023@ogrenci.ogu.edu.tr>
2021-08-11 18:25:15 +0300

    Update auth.interceptor.ts

--- northwind/src/app/interceptors/auth.interceptor.ts
+++ northwind/src/app/interceptors/auth.interceptor.ts
@@ -3,21 +3,25 @@ import {
   HttpRequest,
   HttpHandler,
   HttpEvent,
-  HttpInterceptor
+  HttpInterceptor,
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
-
   constructor() {}
 
-  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+  intercept(
+    request: HttpRequest<unknown>,
+    next: HttpHandler
+  ): Observable<HttpEvent<unknown>> {
     let token = localStorage.getItem("token");
-    let newRequest : HttpRequest<any>;
+    let newRequest: HttpRequest<any>;
     newRequest = request.clone({
-      headers:request.headers.set("Authorization","Bearer "+ token)
-    })
+      headers: request.headers.set('Authorization', 'Bearer ' + token),
+    });
+    console.log(newRequest);
+    console.log(token);
     return next.handle(newRequest);
   }
 }
